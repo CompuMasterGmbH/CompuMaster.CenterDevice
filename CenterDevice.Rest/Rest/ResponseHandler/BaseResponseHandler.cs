91411709f480a12f49bc50013d5731625af242a2
Jochen Wezel <jwezel@compumaster.de>
2022-04-13 18:50:16 +0200

    removed obsolete workaround code parts since RestSharp v107 now correctly handles/deserializes responses with status code NoContent and response body = empty string (not null!)

--- CenterDevice.Rest/Rest/ResponseHandler/BaseResponseHandler.cs
+++ CenterDevice.Rest/Rest/ResponseHandler/BaseResponseHandler.cs
@@ -15,12 +15,7 @@ namespace CenterDevice.Rest.ResponseHandler
 
         public virtual void ValidateResponse(RestResponse result)
         {
-            if (result.ErrorException != null && result.StatusCode == HttpStatusCode.NoContent && result.ErrorException is System.Runtime.Serialization.SerializationException)
-            {
-                result.ErrorMessage = null;
-                result.ErrorException = null;
-            }
-            else if (result.ErrorException != null)
+            if (result.ErrorException != null)
             {
                 if (result.ErrorException is SerializationException)
                 {
